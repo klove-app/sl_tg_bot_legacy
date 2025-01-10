92b393f88c161c1fb4a44ec79cd2e9abc71ba50f
kloveapp <ivankazakov.newl@gmail.com>
2025-01-10 17:40:54 +0100

    feat: add script for adding chat_type column

--- add_column.py
+++ add_column.py
@@ -6,8 +6,8 @@ cursor = conn.cursor()
 try:
     cursor.execute('ALTER TABLE users ADD COLUMN chat_type VARCHAR DEFAULT "group" NOT NULL')
     conn.commit()
-    print("Column chat_type added successfully")
+    print('Column chat_type added successfully')
 except sqlite3.OperationalError as e:
-    print(f"Error: {e}")
+    print(f'Error: {e}')
 finally:
-    conn.close() 
+    conn.close()
