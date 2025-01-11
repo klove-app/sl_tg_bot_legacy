8ec18c402a05c737fe888e48c8f2adf46c48939a
kloveapp <ivankazakov.newl@gmail.com>
2025-01-11 20:59:24 +0100

    Исправлен API ключ Stability AI

--- config/config.py
+++ config/config.py
@@ -3,4 +3,9 @@ ADMIN_IDS = ['1431390352']
 
 # Настройки базы данных
 DATABASE_NAME = 'running_bot.db'  # База данных в корне проекта
-DATABASE_URL = f'sqlite:///{DATABASE_NAME}' 
+DATABASE_URL = f'sqlite:///{DATABASE_NAME}'
+
+# Настройки Stability AI
+STABILITY_API_HOST = 'https://api.stability.ai'
+STABILITY_API_KEY = 'sk-R4NRtaGn4CoNOgtOlFRwJ1I1W0PjjpMFOI8gcC7lLwY5pLn4'
+STABLE_DIFFUSION_ENGINE_ID = 'stable-diffusion-xl-1024-v1-0' 
