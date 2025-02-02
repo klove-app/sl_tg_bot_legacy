e528bc36faa2fa75d468879f6ac3f98a050e1ff8
kloveapp <ivankazakov.newl@gmail.com>
2025-02-02 15:48:58 +0100

    feat: улучшена настройка логирования, добавлено логирование SQL

--- sl_tg_bot_legacy/database/logger.py
+++ sl_tg_bot_legacy/database/logger.py
@@ -1,6 +1,9 @@
 import logging
 
-# Настраиваем логгер
+# Настраиваем корневой логгер
+logging.basicConfig(level=logging.DEBUG)
+
+# Настраиваем логгер для нашего приложения
 logger = logging.getLogger('running_bot')
 logger.setLevel(logging.DEBUG)
 
@@ -8,9 +11,18 @@ logger.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 
-# Создаем форматтер
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# Создаем форматтер с дополнительной информацией
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s'
+)
 console_handler.setFormatter(formatter)
 
+# Удаляем существующие обработчики
+for handler in logger.handlers[:]:
+    logger.removeHandler(handler)
+
 # Добавляем обработчик к логгеру
-logger.addHandler(console_handler) 
+logger.addHandler(console_handler)
+
+# Включаем логирование SQL-запросов
+logging.getLogger('sqlalchemy.engine').setLevel(logging.DEBUG) 
